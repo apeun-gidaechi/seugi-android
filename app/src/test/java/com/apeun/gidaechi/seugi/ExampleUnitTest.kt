41155ae8e180e8fa808a349da282523fc144c778
8954sood <8954sood@naver.com>
2024-03-26 21:22:34 +0900

    Fix: Add Android Spotless Config

--- app/src/test/java/com/apeun/gidaechi/seugi/ExampleUnitTest.kt
+++ app/src/test/java/com/apeun/gidaechi/seugi/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.apeun.gidaechi.seugi
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
