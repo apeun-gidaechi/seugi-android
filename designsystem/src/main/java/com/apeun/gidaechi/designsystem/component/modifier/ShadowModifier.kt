a1dded88fedeff881ca359497b8fe10eac70541a
8954sood <8954sood@naver.com>
2024-03-27 14:19:59 +0900

    style: Apply Spotless Format

--- designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/modifier/ShadowModifier.kt
+++ designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/modifier/ShadowModifier.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -26,11 +25,11 @@ import com.apeun.gidaechi.designsystem.theme.White
 sealed class DropShadowType(
     val blur: Dp,
     val offsetY: Dp,
-    val color: Color
+    val color: Color,
 ) {
-    data object Ev1: DropShadowType(9.dp, 3.dp, Color(0x08000000))
-    data object Ev2: DropShadowType(12.dp, 4.dp, Color(0x0F000000))
-    data object Ev3: DropShadowType(18.dp, 6.dp, Color(0x14000000))
+    data object Ev1 : DropShadowType(9.dp, 3.dp, Color(0x08000000))
+    data object Ev2 : DropShadowType(12.dp, 4.dp, Color(0x0F000000))
+    data object Ev3 : DropShadowType(18.dp, 6.dp, Color(0x14000000))
 }
 
 /**
@@ -39,10 +38,7 @@ sealed class DropShadowType(
  * @param type: the size of the shadow according to the design system.
  * @param modifier: the Modifier to be applied to this drop shadow
  */
-fun Modifier.dropShadow(
-    type: DropShadowType,
-    modifier: Modifier = Modifier,
-) = then(
+fun Modifier.dropShadow(type: DropShadowType, modifier: Modifier = Modifier) = then(
     modifier.drawBehind {
         drawIntoCanvas { canvas ->
             type.run {
@@ -59,7 +55,7 @@ fun Modifier.dropShadow(
                 if (type.blur != 0.dp) {
                     frameworkPaint.maskFilter = BlurMaskFilter(
                         type.blur.toPx(),
-                        BlurMaskFilter.Blur.NORMAL
+                        BlurMaskFilter.Blur.NORMAL,
                     )
                 }
 
@@ -70,11 +66,11 @@ fun Modifier.dropShadow(
                     bottom = bottomPixel,
                     radiusX = 0f,
                     radiusY = 0f,
-                    paint = paint
+                    paint = paint,
                 )
             }
         }
-    }
+    },
 )
 
 @Composable
@@ -82,27 +78,27 @@ fun Modifier.dropShadow(
 private fun ShadowModifierPreview() {
     SeugiTheme {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         ) {
             Box(
                 modifier = Modifier
                     .size(50.dp)
                     .dropShadow(DropShadowType.Ev1)
-                    .background(White)
+                    .background(White),
             )
             Spacer(modifier = Modifier.height(30.dp))
             Box(
                 modifier = Modifier
                     .size(50.dp)
                     .dropShadow(DropShadowType.Ev2)
-                    .background(White)
+                    .background(White),
             )
             Spacer(modifier = Modifier.height(30.dp))
             Box(
                 modifier = Modifier
                     .size(50.dp)
                     .dropShadow(DropShadowType.Ev3)
-                    .background(White)
+                    .background(White),
             )
         }
     }
