c2a70bb36fc8c61eb55fa698ae35ba4bcdc896c3
8954sood <8954sood@naver.com>
2024-03-27 19:19:23 +0900

    chore: Add Button Type Color

--- designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/Button.kt
+++ designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/Button.kt
@@ -1,18 +1,25 @@
 package com.apeun.gidaechi.designsystem.component
 
 import androidx.compose.ui.graphics.Color
+import com.apeun.gidaechi.designsystem.theme.Gray500
+import com.apeun.gidaechi.designsystem.theme.Primary100
 import com.apeun.gidaechi.designsystem.theme.Primary500
+import com.apeun.gidaechi.designsystem.theme.Red100
 import com.apeun.gidaechi.designsystem.theme.Red200
+import com.apeun.gidaechi.designsystem.theme.Red300
 import com.apeun.gidaechi.designsystem.theme.Red500
+import com.apeun.gidaechi.designsystem.theme.Transparent
 import com.apeun.gidaechi.designsystem.theme.White
 
 sealed class ButtonType(
     val textColor: Color,
-    val backgroundColor: Color
+    val backgroundColor: Color,
+    val disableTextColor: Color,
+    val disableBackgroundColor: Color
 ) {
-    data object Primary: ButtonType(White, Primary500)
-    data object Black: ButtonType(White, com.apeun.gidaechi.designsystem.theme.Black)
-    data object Red: ButtonType(Red500, Red200)
-    data object Transparent: ButtonType(com.apeun.gidaechi.designsystem.theme.Black, White)
-    data object Shadow: ButtonType(com.apeun.gidaechi.designsystem.theme.Black, White)
+    data object Primary: ButtonType(White, Primary500, White, Primary100)
+    data object Black: ButtonType(White, com.apeun.gidaechi.designsystem.theme.Black, White, Gray500)
+    data object Red: ButtonType(Red500, Red200, Red300, Red100)
+    data object Transparent: ButtonType(com.apeun.gidaechi.designsystem.theme.Black, com.apeun.gidaechi.designsystem.theme.Transparent, Gray500, com.apeun.gidaechi.designsystem.theme.Transparent)
+    data object Shadow: ButtonType(com.apeun.gidaechi.designsystem.theme.Black, White, Gray500, White)
 }
