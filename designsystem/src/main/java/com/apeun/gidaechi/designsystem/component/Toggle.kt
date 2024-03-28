53fe184f5f0fc92ed8a9bdc7177b570ddefcec70
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-03-28 20:32:08 +0900

    style: Apply Spotless Format

--- designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/Toggle.kt
+++ designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/Toggle.kt
@@ -37,22 +37,15 @@ fun Preview() {
             .fillMaxSize()
             .background(Color.Black),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Toggle(checked = true)
     }
 }
 
-
 @Composable
-fun Toggle(
-    checked: Boolean,
-    modifier: Modifier = Modifier
-) {
-
+fun Toggle(checked: Boolean, modifier: Modifier = Modifier) {
     var isChecked by remember { mutableStateOf(checked) }
-
-
 
     SeugiTheme {
         Switch(
@@ -63,20 +56,21 @@ fun Toggle(
                 checkedTrackColor = Primary500,
                 uncheckedThumbColor = White,
                 uncheckedTrackColor = Gray200,
-                uncheckedBorderColor = Transparent
+                uncheckedBorderColor = Transparent,
 
             ),
             modifier = Modifier
                 .height(31.dp)
                 .width(51.dp),
             thumbContent = {
-                Box(modifier = Modifier
-                    .size(27.dp)
-                    .background(
-                        color = White,
-                        shape = CircleShape
-                    )
-                    .dropShadow(DropShadowType.Ev2)
+                Box(
+                    modifier = Modifier
+                        .size(27.dp)
+                        .background(
+                            color = White,
+                            shape = CircleShape,
+                        )
+                        .dropShadow(DropShadowType.Ev2),
                 )
             },
         )
