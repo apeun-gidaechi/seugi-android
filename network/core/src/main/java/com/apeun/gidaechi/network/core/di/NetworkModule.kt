c9b54e7226aa8314b087226d111c2b0192f139a2
8954sood <58807462+8954sood@users.noreply.github.com>
2024-04-07 21:17:19 +0900

    feat: Create Network Core Module

---
+++ network/core/src/main/java/com/apeun/gidaechi/network/core/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package com.apeun.gidaechi.network.core.di
+
+import android.util.Log
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.accept
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient =
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    },
+                )
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.i("HttpClient", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = TIME_OUT
+                connectTimeoutMillis = TIME_OUT
+                socketTimeoutMillis = TIME_OUT
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                accept(ContentType.Application.Json)
+            }
+        }
+}
+
+private const val TIME_OUT = 60_000L
