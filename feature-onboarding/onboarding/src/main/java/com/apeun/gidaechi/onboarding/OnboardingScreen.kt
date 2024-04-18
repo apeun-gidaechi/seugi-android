5396c865b6cd93afc13dd59db13903aca3f1ab39
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-04-18 18:08:06 +0900

    style: Apply Spotless Format

--- feature-onboarding/onboarding/src/main/java/com/apeun/gidaechi/onboarding/OnboardingScreen.kt
+++ feature-onboarding/onboarding/src/main/java/com/apeun/gidaechi/onboarding/OnboardingScreen.kt
@@ -10,23 +10,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apeun.gidaechi.designsystem.component.ButtonType
@@ -35,12 +32,10 @@ import com.apeun.gidaechi.designsystem.component.SeugiOAuthButton
 import com.apeun.gidaechi.designsystem.theme.Gradient
 import com.apeun.gidaechi.designsystem.theme.SeugiTheme
 import com.apeun.gidaechi.designsystem.theme.White
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun OnboardingScreen() {
-
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
@@ -48,42 +43,40 @@ internal fun OnboardingScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Brush.verticalGradient(Gradient))
+            .background(Brush.verticalGradient(Gradient)),
     ) {
         Spacer(modifier = Modifier.weight(1f))
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.End
+            horizontalArrangement = Arrangement.End,
         ) {
             Image(
                 painter = painterResource(id = com.apeun.gidaechi.designsystem.R.drawable.image_cloud1),
-                contentDescription = ""
+                contentDescription = "",
             )
-
         }
         Spacer(modifier = Modifier.weight(1f))
         Column(
-            modifier = Modifier.padding(start = 24.dp)
+            modifier = Modifier.padding(start = 24.dp),
         ) {
             Text(text = "스기", style = MaterialTheme.typography.displayLarge, color = White)
             Text(
                 text = "학생, 선생님 모두 함께하는\n스마트 스쿨 플랫폼",
                 style = MaterialTheme.typography.titleMedium,
-                color = White
+                color = White,
             )
         }
         Spacer(modifier = Modifier.weight(1f))
 
-
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Start,
         ) {
             Image(
                 painter = painterResource(id = com.apeun.gidaechi.designsystem.R.drawable.image_cloud2),
-                contentDescription = ""
+                contentDescription = "",
             )
         }
         Spacer(modifier = Modifier.weight(1f))
@@ -91,10 +84,9 @@ internal fun OnboardingScreen() {
             onClick = { showBottomSheet = true },
             type = ButtonType.Transparent,
             text = "시작하기",
-            modifier = Modifier.padding(horizontal = 20.dp)
+            modifier = Modifier.padding(horizontal = 20.dp),
         )
         Spacer(modifier = Modifier.weight(1f))
-
 
         if (showBottomSheet) {
             ModalBottomSheet(
@@ -103,21 +95,25 @@ internal fun OnboardingScreen() {
                 },
                 sheetState = sheetState,
                 containerColor = White,
-                dragHandle = null
+                dragHandle = null,
             ) {
                 Column(
                     modifier = Modifier
-                        .padding(20.dp)
+                        .padding(20.dp),
                 ) {
-                    Column{
+                    Column {
                         SeugiFullWidthButton(
                             onClick = { /*TODO*/ },
                             type = ButtonType.Black,
-                            text = "이메일로 계속하기"
+                            text = "이메일로 계속하기",
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         // TODO: google icon
-                        SeugiOAuthButton(image = com.apeun.gidaechi.designsystem.R.drawable.ic_menu, text = "Google로 계속하기", onClick = { /*TODO: google login*/ })
+                        SeugiOAuthButton(
+                            image = com.apeun.gidaechi.designsystem.R.drawable.ic_menu,
+                            text = "Google로 계속하기",
+                            onClick = { /*TODO: google login*/ },
+                        )
                     }
                     Spacer(modifier = Modifier.height(50.dp))
                 }
