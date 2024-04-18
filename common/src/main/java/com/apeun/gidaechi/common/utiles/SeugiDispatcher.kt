6c52c61ce02a911b8e01cf657d477042b623699b
8954sood <8954sood@naver.com>
2024-04-18 19:35:02 +0900

    feat: Create Dispatcher Di

---
+++ common/src/main/java/com/apeun/gidaechi/common/utiles/SeugiDispatcher.kt
@@ -0,0 +1,13 @@
+package com.apeun.gidaechi.common.utiles
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SeugiDispatcher(val dispatcherType: DispatcherType)
+
+enum class DispatcherType {
+    Default,
+    IO,
+    Main
+}
