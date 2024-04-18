516bbbab6f5c9cf96f9cb51ba8b2dd1a6825e4ab
8954sood <8954sood@naver.com>
2024-04-19 08:34:30 +0900

    feat: Create Result

---
+++ common/src/main/java/com/apeun/gidaechi/common/model/Result.kt
@@ -0,0 +1,18 @@
+package com.apeun.gidaechi.common.model
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+sealed interface Result<out T> {
+    data class Success<T>(val data: T): Result<T>
+    data class Error(val throwable: Throwable): Result<Nothing>
+    data object Loading: Result<Nothing>
+}
+
+fun <T> Flow<T>.asResult(): Flow<Result<T>> = map<T, Result<T>> { Result.Success(it) }
+    .onStart { emit(Result.Loading) }
+    .catch {  error ->
+        emit(Result.Error(error))
+    }
