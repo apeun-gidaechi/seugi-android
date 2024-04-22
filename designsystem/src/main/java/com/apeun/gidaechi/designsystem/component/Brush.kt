5b29875855df25680e7965dc80144a548bcca513
8954sood <8954sood@naver.com>
2024-04-22 17:08:41 +0900

    feat: Create Design System Gradient Primary Brush

---
+++ designsystem/src/main/java/com/apeun/gidaechi/designsystem/component/Brush.kt
@@ -0,0 +1,8 @@
+package com.apeun.gidaechi.designsystem.component
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val GradientPrimary = Brush.verticalGradient(
+    colors = listOf(Color(0xFF1C8DF4), Color(0xFF21B6E5))
+)
