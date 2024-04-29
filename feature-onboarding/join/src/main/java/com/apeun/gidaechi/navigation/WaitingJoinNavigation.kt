e8bd15a467d18034188586c750a4b3d5addd3601
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-04-29 15:25:03 +0900

    feat: Create WaitingJoin Navigation

---
+++ feature-onboarding/join/src/main/java/com/apeun/gidaechi/navigation/WaitingJoinNavigation.kt
@@ -0,0 +1,18 @@
+package com.apeun.gidaechi.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.apeun.gidaechi.WaitingJoinScreen
+
+const val WAITING_JOIN = "waitingJoin"
+
+fun NavController.navigateToWaitingJoin(navOptions: NavOptions? = null) = navigate(
+    WAITING_JOIN, navOptions)
+
+fun NavGraphBuilder.waitingJoin() {
+    composable(route = WAITING_JOIN) {
+        WaitingJoinScreen()
+    }
+}
