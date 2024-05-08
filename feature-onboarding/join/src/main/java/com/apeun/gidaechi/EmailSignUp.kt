bb270448f95eab0f25206adc077e1ad2895bed4b
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-05-08 16:25:53 +0900

    style: Apply Spotless Format

--- feature-onboarding/join/src/main/java/com/apeun/gidaechi/EmailSignUp.kt
+++ feature-onboarding/join/src/main/java/com/apeun/gidaechi/EmailSignUp.kt
@@ -1,7 +1,5 @@
 package com.apeun.gidaechi
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,8 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apeun.gidaechi.designsystem.component.ButtonType
 import com.apeun.gidaechi.designsystem.component.SeugiFullWidthButton
@@ -180,10 +176,7 @@ internal fun EmailSignUpScreen(navigateToEmailVerification: () -> Unit, popBackS
                     modifier = Modifier
                         .padding(vertical = 16.dp),
                 )
-
             }
-
-
         }
     }
 }
