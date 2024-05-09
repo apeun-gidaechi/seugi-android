e26945722b9fd502a991cc5fd3bb31f47223dfe9
8954sood <8954sood@naver.com>
2024-05-09 17:04:26 +0900

    chore: Edit String To Response Mapper

--- network/core/src/main/java/com/apeun/gidaechi/network/core/utiles/Extension.kt
+++ network/core/src/main/java/com/apeun/gidaechi/network/core/utiles/Extension.kt
@@ -1,11 +1,25 @@
 package com.apeun.gidaechi.network.core.utiles
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.time.LocalDateTime
 
 inline fun <reified T> String.toResponse(): T {
-    return Gson().fromJson(this, T::class.java)
+    return GsonConverter.gson.fromJson(this, T::class.java)
 }
 
 inline fun <reified T> String.toResponse(type: Class<T>): T {
-    return Gson().fromJson(this, type)
+    return GsonConverter.gson.fromJson(this, type)
+}
+
+
+object GsonConverter {
+
+    val gson: Gson by lazy {
+        GsonBuilder()
+            .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeTypeAdapter())
+            .setPrettyPrinting()
+            .setLenient()
+            .create()
+    }
 }
