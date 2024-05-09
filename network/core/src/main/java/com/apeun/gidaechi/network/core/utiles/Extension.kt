a1cde37ed79823b8999dae57d18b26acad1501e5
8954sood <8954sood@naver.com>
2024-05-09 17:18:53 +0900

    feat: Create Data Class To Json String

--- network/core/src/main/java/com/apeun/gidaechi/network/core/utiles/Extension.kt
+++ network/core/src/main/java/com/apeun/gidaechi/network/core/utiles/Extension.kt
@@ -12,6 +12,20 @@ inline fun <reified T> String.toResponse(type: Class<T>): T {
     return GsonConverter.gson.fromJson(this, type)
 }
 
+/**
+ * Data Class To Json String
+ *
+ * @param T Only data class
+ * @return String
+ */
+inline fun <reified T> T.toJsonString(): String {
+    if (T::class.isData) {
+        return GsonConverter.gson.toJson(this)
+    } else {
+        throw IllegalArgumentException("Only data classes are allowed")
+    }
+}
+
 
 object GsonConverter {
 
