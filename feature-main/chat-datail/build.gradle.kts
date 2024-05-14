19660cdff825db3577454715e9ad7ea2e0845039
8954sood <8954sood@naver.com>
2024-05-14 17:31:16 +0900

    build: Add Chat Detail Feature Layer Implementation With Data Layer

--- feature-main/chat-datail/build.gradle.kts
+++ feature-main/chat-datail/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies {
 
     implementation(projects.designsystem)
     implementation(projects.common)
+    implementation(projects.data.chatDetail)
+    implementation(libs.kotlinx.collections.immutable)
 }
