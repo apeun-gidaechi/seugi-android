03e619bbb4ffc9420f6ce6c0a711d9cd7f13a9e3
8954sood <8954sood@naver.com>
2024-05-16 19:36:13 +0900

    build: Rename Chat Detail Data Layer NameSpace

--- data/chat-detail/build.gradle.kts
+++ data/chat-detail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.apeun.gidaechi.chatdetail"
+    namespace = "com.apeun.gidaechi.data.chatdetail"
     defaultConfig {
         consumerProguardFiles("consumer-rules.pro")
     }
