6d6cd091d2f931424cf05b69db01eb119d5531cd
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-05-24 15:15:48 +0900

    style: Apply Spotless Format

--- feature-main/chat-datail/src/main/java/com/apeun/gidaechi/chatdatail/ChatDetailScreen.kt
+++ feature-main/chat-datail/src/main/java/com/apeun/gidaechi/chatdatail/ChatDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.apeun.gidaechi.chatdatail
 
 import android.graphics.Rect
-import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.activity.compose.BackHandler
