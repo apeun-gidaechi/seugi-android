f482dd9ac4bea7383ad012df3868a36bc25ffb6f
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-05-24 13:40:47 +0900

    remove: Remove Const Val

--- network/core/src/main/java/com/apeun/gidaechi/network/core/SeugiUrl.kt
+++ network/core/src/main/java/com/apeun/gidaechi/network/core/SeugiUrl.kt
@@ -1,16 +1,16 @@
 package com.apeun.gidaechi.network.core
 
 object SeugiUrl {
-    private const val BASE_URL = BuildConfig.BASE_URL
+    private val BASE_URL = BuildConfig.BASE_URL
 
     object Chat {
-        private const val WS_URL = BuildConfig.WS_URL
-        const val HANDSHAKE = "${WS_URL}/stomp/chat"
-        const val SUBSCRIPTION = "/exchange/chat.exchange/room."
-        const val VIRTUALHOST = "/"
+        private val WS_URL = BuildConfig.WS_URL
+        val HANDSHAKE = "${WS_URL}/stomp/chat"
+        val SUBSCRIPTION = "/exchange/chat.exchange/room."
+        val VIRTUALHOST = "/"
     }
 
     object Auth {
-        const val EMAIL_SIGN_IN = "$BASE_URL/member/login"
+        val EMAIL_SIGN_IN = "$BASE_URL/member/login"
     }
 }
