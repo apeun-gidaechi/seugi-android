587808ff94f597132cba3b542e3e554fbecf7553
8954sood <8954sood@naver.com>
2024-06-12 23:14:50 +0900

    style: Apply Spotless Format

--- feature-main/chat-datail/src/main/java/com/apeun/gidaechi/chatdatail/navigation/ChatDetailNavigation.kt
+++ feature-main/chat-datail/src/main/java/com/apeun/gidaechi/chatdatail/navigation/ChatDetailNavigation.kt
@@ -1,6 +1,5 @@
 package com.apeun.gidaechi.chatdatail.navigation
 
-import androidx.navigation.NavArgument
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
@@ -11,24 +10,29 @@ import com.apeun.gidaechi.chatdatail.ChatDetailScreen
 
 const val CHAT_DETAIL_ROUTE = "chat_detail"
 
-fun NavController.navigateToChatDetail(workspace: String = "664bdd0b9dfce726abd30462", isPersonal: Boolean = false, chatRoomId: String = "66698a18d3f6963445f6f84f", navOptions: NavOptions? = null) = navigate("${CHAT_DETAIL_ROUTE}/${workspace}/${isPersonal}/${chatRoomId}", navOptions)
+fun NavController.navigateToChatDetail(
+    workspace: String = "664bdd0b9dfce726abd30462",
+    isPersonal: Boolean = false,
+    chatRoomId: String = "66698a18d3f6963445f6f84f",
+    navOptions: NavOptions? = null,
+) = navigate(
+    "${CHAT_DETAIL_ROUTE}/$workspace/$isPersonal/$chatRoomId",
+    navOptions,
+)
 
-fun NavGraphBuilder.chatDetailScreen(
-    onNavigationVisibleChange: (Boolean) -> Unit,
-    popBackStack: () -> Unit
-) {
+fun NavGraphBuilder.chatDetailScreen(onNavigationVisibleChange: (Boolean) -> Unit, popBackStack: () -> Unit) {
     composable(
         route = "${CHAT_DETAIL_ROUTE}/{workspace}/{isPersonal}/{chatRoomId}",
         arguments = listOf(
-            navArgument("workspace") { NavType.StringType},
-            navArgument("isPersonal") { NavType.BoolType},
-            navArgument("chatRoomId") { NavType.StringType},
-        )
+            navArgument("workspace") { NavType.StringType },
+            navArgument("isPersonal") { NavType.BoolType },
+            navArgument("chatRoomId") { NavType.StringType },
+        ),
     ) {
         ChatDetailScreen(
-            workspace = it.arguments?.getString("workspace")?: "",
+            workspace = it.arguments?.getString("workspace") ?: "",
             isPersonal = it.arguments?.getString("isPersonal") == "true",
-            chatRoomId = it.arguments?.getString("chatRoomId")?: "",
+            chatRoomId = it.arguments?.getString("chatRoomId") ?: "",
             onNavigationVisibleChange = onNavigationVisibleChange,
             popBackStack = popBackStack,
         )
