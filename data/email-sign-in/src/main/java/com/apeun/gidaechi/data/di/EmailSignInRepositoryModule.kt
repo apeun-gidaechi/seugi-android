8af92eac6a6457766dfce5b5d74ca076784f4286
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-06-12 13:42:57 +0900

    feat: Singleton Apply

--- data/email-sign-in/src/main/java/com/apeun/gidaechi/data/di/EmailSignInRepositoryModule.kt
+++ data/email-sign-in/src/main/java/com/apeun/gidaechi/data/di/EmailSignInRepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface EmailSignInRepositoryModule {
 
     @Binds
+    @Singleton
     fun provideEmailSignInRepository(emailSignInRepositoryImpl: EmailSignInRepositoryImpl): EmailSignInRepository
 }
