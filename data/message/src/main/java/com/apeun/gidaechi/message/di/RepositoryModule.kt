932bd28bd48428a8c04a656a56ca70e780cd017a
8954sood <8954sood@naver.com>
2024-06-13 17:39:59 +0900

    chore: Add Message Repository Di Singleton

--- data/message/src/main/java/com/apeun/gidaechi/message/di/RepositoryModule.kt
+++ data/message/src/main/java/com/apeun/gidaechi/message/di/RepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface RepositoryModule {
 
+    @Singleton
     @Binds
     fun provideMessageRepository(chatDetailRepositoryImpl: MessageRepositoryImpl): MessageRepository
 }
