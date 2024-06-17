5f1cd5a227dd4888bec2d3029848b4ca79fdf722
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-06-17 18:20:21 +0900

    style: Apply Spotless Format

--- feature-onboarding/join/src/main/java/com/apeun/gidaechi/model/EmailSignUpSideEffect.kt
+++ feature-onboarding/join/src/main/java/com/apeun/gidaechi/model/EmailSignUpSideEffect.kt
@@ -2,8 +2,8 @@ package com.apeun.gidaechi.model
 
 sealed class EmailSignUpSideEffect {
     data object Success : EmailSignUpSideEffect()
-    data object FailedVeritication: EmailSignUpSideEffect()
+    data object FailedVeritication : EmailSignUpSideEffect()
     data object FailedJoin : EmailSignUpSideEffect()
-    data object SuccessGetCode: EmailSignUpSideEffect()
-    data object FailedGetCode: EmailSignUpSideEffect()
+    data object SuccessGetCode : EmailSignUpSideEffect()
+    data object FailedGetCode : EmailSignUpSideEffect()
 }
