34bac58bfe91c83c625bcf159079bd89c2d3854c
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-06-18 17:14:47 +0900

    chore: Change Module Name

--- data/email-sign-in/src/main/java/com/apeun/gidaechi/data/di/MemberRepositoryModule.kt
+++ data/email-sign-in/src/main/java/com/apeun/gidaechi/data/di/MemberRepositoryModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface EmailSignInRepositoryModule {
+interface MemberRepositoryModule {
 
     @Binds
     @Singleton
