2b40431d3e4eb377ac0c788203a869685bab395b
8954sood <8954sood@naver.com>
2024-06-26 09:58:45 +0900

    style: Apply Spotless Format

--- designsystem-preview/src/main/java/com/apeun/gidaechi/designsystem/preview/feature/RoomImage.kt
+++ designsystem-preview/src/main/java/com/apeun/gidaechi/designsystem/preview/feature/RoomImage.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.apeun.gidaechi.designsystem.component.RoomImageType
 import com.apeun.gidaechi.designsystem.component.SeugiRoomImage
 
