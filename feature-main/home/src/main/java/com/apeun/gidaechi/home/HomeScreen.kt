7abb2fb0ff62159f777d3898f675ad5d1b8ffc2e
8954sood <8954sood@naver.com>
2024-06-26 17:27:24 +0900

    feat: Create Home Screen Meal View

--- feature-main/home/src/main/java/com/apeun/gidaechi/home/HomeScreen.kt
+++ feature-main/home/src/main/java/com/apeun/gidaechi/home/HomeScreen.kt
@@ -8,9 +8,15 @@ import android.view.View
 import android.view.Window
 import android.view.WindowInsetsController
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
@@ -20,10 +26,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -33,7 +42,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +54,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
@@ -51,6 +65,8 @@ import androidx.lifecycle.compose.LifecycleResumeEffect
 import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter.State.Empty.painter
 import com.apeun.gidaechi.designsystem.R
+import com.apeun.gidaechi.designsystem.animation.ButtonState
+import com.apeun.gidaechi.designsystem.animation.NoInteractionSource
 import com.apeun.gidaechi.designsystem.animation.bounceClick
 import com.apeun.gidaechi.designsystem.component.ButtonType
 import com.apeun.gidaechi.designsystem.component.DividerType
@@ -61,8 +77,10 @@ import com.apeun.gidaechi.designsystem.component.modifier.DropShadowType
 import com.apeun.gidaechi.designsystem.component.modifier.dropShadow
 import com.apeun.gidaechi.designsystem.theme.Black
 import com.apeun.gidaechi.designsystem.theme.Gray100
+import com.apeun.gidaechi.designsystem.theme.Gray300
 import com.apeun.gidaechi.designsystem.theme.Gray500
 import com.apeun.gidaechi.designsystem.theme.Gray600
+import com.apeun.gidaechi.designsystem.theme.Gray700
 import com.apeun.gidaechi.designsystem.theme.Primary050
 import com.apeun.gidaechi.designsystem.theme.Primary100
 import com.apeun.gidaechi.designsystem.theme.Primary200
@@ -70,12 +88,16 @@ import com.apeun.gidaechi.designsystem.theme.Primary300
 import com.apeun.gidaechi.designsystem.theme.Primary500
 import com.apeun.gidaechi.designsystem.theme.White
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 internal fun HomeScreen() {
     val view = LocalView.current
+    val pagerState = rememberPagerState() { 3 }
     val items = (1..7).toList()
     val selectIndex = 4
+    val indicatorOffset by remember {
+        derivedStateOf { (pagerState.currentPage*10).dp }
+    }
 
     LifecycleResumeEffect(Unit) {
         onPauseOrDispose {
@@ -201,6 +223,114 @@ internal fun HomeScreen() {
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(8.dp))
+            var nowButtonState by remember { mutableStateOf(ButtonState.Idle) }
+            HomeCard(
+                text = "오늘의 급식",
+                image = painterResource(id = R.drawable.ic_utensils_line),
+                colorFilter = ColorFilter.tint(Gray600),
+                onClickDetail = {},
+                onChangeButtonState = {
+                    nowButtonState = it
+                }
+            ) {
+                Column {
+                    HorizontalPager(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(
+                                interactionSource = NoInteractionSource(),
+                                indication = null,
+                                onClick = {}
+                            )
+                            .pointerInput(Unit) { },
+                        state = pagerState
+                    ) { index ->
+                        Row {
+                            Column(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(vertical = (6.5).dp),
+                                    text = "오리훈제볶음밥\n간장두조림\n배추김치\n초코첵스시리얼+우유\n오렌지",
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = Gray700
+                                )
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .background(
+                                            color = Primary500,
+                                            shape = RoundedCornerShape(34.dp)
+                                        )
+                                ) {
+                                    Text(
+                                        modifier = Modifier.padding(
+                                            vertical = 4.dp,
+                                            horizontal = 8.dp
+                                        ),
+                                        text = "872Kcal",
+                                        style = MaterialTheme.typography.labelLarge,
+                                        color = White
+                                    )
+                                }
+                            }
+                            Box(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Column(
+                                    modifier = Modifier.align(Alignment.Center)
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(94.dp),
+                                        painter = painterResource(id = R.drawable.ic_book_fill),
+                                        contentDescription = ""
+                                    )
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Text(
+                                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                                        text = when (index) {
+                                            0 -> "아침"
+                                            1 -> "점심"
+                                            else -> "저녁"
+                                        },
+                                        style = MaterialTheme.typography.labelLarge,
+                                        color = Gray500
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Box(
+                        modifier = Modifier
+                            .width(36.dp)
+                            .height(6.dp)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(
+                                    color = Gray300,
+                                    shape = RoundedCornerShape(4.dp)
+                                )
+                        )
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.TopStart)
+                                .offset(x = indicatorOffset)
+                                .width(16.dp)
+                                .fillMaxHeight()
+                                .background(
+                                    color = Primary500,
+                                    shape = RoundedCornerShape(4.dp)
+                                )
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -282,12 +412,15 @@ internal fun HomeCard(
     onClickDetail: () -> Unit,
     image: Painter,
     colorFilter: ColorFilter? = null,
+    onChangeButtonState: (ButtonState) -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     Box(
         modifier = Modifier
             .bounceClick(
-                onClick = onClickDetail
+                onClick = onClickDetail,
+                onChangeButtonState = onChangeButtonState,
+                requireUnconsumed = true
             )
     ) {
         Column(
