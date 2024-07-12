d486518a5f8db4bae87b7f79cdc513ac9f626aef
8954sood <8954sood@naver.com>
2024-07-12 10:21:52 +0900

    feat: Create Room Search Featrue

--- feature-main/room/src/main/java/com/apeun/gidaechi/room/model/RoomUiState.kt
+++ feature-main/room/src/main/java/com/apeun/gidaechi/room/model/RoomUiState.kt
@@ -3,7 +3,15 @@ package com.apeun.gidaechi.room.model
 import com.apeun.gidaechi.data.core.model.ChatRoomModel
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 
 data class RoomUiState(
-    val chatItems: ImmutableList<ChatRoomModel> = persistentListOf(),
-)
+    val filterMessage: String = "",
+    private val _chatItems: ImmutableList<ChatRoomModel> = persistentListOf(),
+) {
+    val chatItems = _chatItems
+        .filter {
+            it.chatName.contains(filterMessage)
+        }
+        .toImmutableList()
+}
