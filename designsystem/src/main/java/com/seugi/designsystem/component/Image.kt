ca6d5b3f34f1714eb30c0a77c3f84a053c7fbc45
8954sood <8954sood@naver.com>
2024-07-26 17:30:56 +0900

    feat: Create Seugi Image Component

---
+++ designsystem/src/main/java/com/seugi/designsystem/component/Image.kt
@@ -0,0 +1,44 @@
+package com.seugi.designsystem.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.res.painterResource
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun SeugiImage(
+    modifier: Modifier = Modifier,
+    url: String,
+    contentDescription: String? = null,
+    colorFilter: ColorFilter? = null,
+    alpha: Float = DefaultAlpha,
+) {
+    Image(
+        modifier = modifier,
+        painter = rememberAsyncImagePainter(model = url),
+        contentDescription = contentDescription,
+        colorFilter = colorFilter,
+        alpha = alpha
+    )
+}
+
+@Composable
+fun SeugiImage(
+    modifier: Modifier = Modifier,
+    @DrawableRes resId: Int,
+    contentDescription: String? = null,
+    colorFilter: ColorFilter? = null,
+    alpha: Float = DefaultAlpha,
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(id = resId),
+        contentDescription = contentDescription,
+        colorFilter = colorFilter,
+        alpha = alpha
+    )
+}
