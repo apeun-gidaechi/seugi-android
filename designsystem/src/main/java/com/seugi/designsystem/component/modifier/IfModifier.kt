2bc9b35e1b1aad98b10d5985e94effe863b369a0
8954sood <8954sood@naver.com>
2024-07-30 20:38:19 +0900

    refactor: Add Inline Keyword In If Modifier

--- designsystem/src/main/java/com/seugi/designsystem/component/modifier/IfModifier.kt
+++ designsystem/src/main/java/com/seugi/designsystem/component/modifier/IfModifier.kt
@@ -2,4 +2,4 @@ package com.seugi.designsystem.component.modifier
 
 import androidx.compose.ui.Modifier
 
-fun Modifier.`if`(enabled: Boolean, modifier: Modifier.() -> Modifier) = if (enabled) modifier() else this
+inline fun Modifier.`if`(enabled: Boolean, modifier: Modifier.() -> Modifier) = if (enabled) modifier() else this
