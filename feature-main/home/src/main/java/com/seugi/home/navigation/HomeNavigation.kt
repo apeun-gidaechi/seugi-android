cef0a72e43fb1ee5bacc2615c5800a96afc0c036
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-03 16:24:39 +0900

    feat: JoinWorkspace To Home

--- feature-main/home/src/main/java/com/seugi/home/navigation/HomeNavigation.kt
+++ feature-main/home/src/main/java/com/seugi/home/navigation/HomeNavigation.kt
@@ -10,6 +10,14 @@ const val HOME_ROUTE = "home"
 
 fun NavController.navigateToHome(navOptions: NavOptions?) = navigate(HOME_ROUTE, navOptions)
 
+fun NavController.navigateToHome(toRoute: String, fromRoute: String) {
+    this.navigate(toRoute) {
+        popUpTo(fromRoute) {
+            inclusive = true
+        }
+    }
+}
+
 fun NavGraphBuilder.homeScreen(navigateToChatSeugi: () -> Unit, navigateToJoinWorkspace: () -> Unit, onNavigationVisibleChange: (Boolean) -> Unit) {
     composable(HOME_ROUTE) {
         HomeScreen(
