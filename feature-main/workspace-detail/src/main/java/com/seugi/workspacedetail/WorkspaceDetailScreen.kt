c2b046adcf15b0d8e2a3ac7c0da4873d74d477a0
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-13 12:05:00 +0900

    refactor: Remove If

--- feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/WorkspaceDetailScreen.kt
+++ feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/WorkspaceDetailScreen.kt
@@ -101,7 +101,7 @@ fun WorkspaceDetailScreen(
                                 color = Gray600
                             )
                             LazyColumn {
-                                itemsIndexed(state.myWorkspace!!) { index, item ->
+                                itemsIndexed(state.myWorkspace) { index, item ->
                                     Row {
                                         Row(
                                             modifier = Modifier
@@ -146,56 +146,56 @@ fun WorkspaceDetailScreen(
 
                             Spacer(modifier = Modifier.height(16.dp))
 
-                            if (state.waitWorkspace != null) {
-                                Text(
-                                    modifier = Modifier.padding(bottom = 4.dp),
-                                    text = "가입 대기 중",
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    color = Gray600
-                                )
-                                LazyColumn {
-                                    itemsIndexed(state.waitWorkspace!!) { index, item ->
-                                        Row {
-                                            Row(
-                                                modifier = Modifier
-                                                    .fillMaxWidth()
-                                                    .height(56.dp)
-                                                    .background(
-                                                        color = Gray100,
-                                                        shape = RoundedCornerShape(8.dp)
+                            if (state.waitWorkspace.isNotEmpty()){
+                            Text(
+                                modifier = Modifier.padding(bottom = 4.dp),
+                                text = "가입 대기 중",
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Gray600
+                            )
+                            }
+                            LazyColumn {
+                                itemsIndexed(state.waitWorkspace) { index, item ->
+                                    Row {
+                                        Row(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .height(56.dp)
+                                                .background(
+                                                    color = Gray100,
+                                                    shape = RoundedCornerShape(8.dp)
+                                                )
+                                                .bounceClick(onClick = {
+                                                    Log.d(
+                                                        "TAG",
+                                                        "${item?.workspaceId}: "
                                                     )
-                                                    .bounceClick(onClick = {
-                                                        Log.d(
-                                                            "TAG",
-                                                            "${item?.workspaceId}: "
-                                                        )
-                                                    }),
-                                                verticalAlignment = Alignment.CenterVertically
-                                            ) {
-                                                Text(
-                                                    modifier = Modifier.padding(start = 16.dp),
-                                                    text = item?.workspaceName ?:"",
-                                                    style = MaterialTheme.typography.titleMedium
-                                                )
-                                                Spacer(modifier = Modifier.weight(1f))
-                                                Image(
-                                                    modifier = Modifier
-                                                        .padding(end = 20.dp)
-                                                        .size(24.dp),
-                                                    painter = painterResource(id = R.drawable.ic_expand_right_line),
-                                                    contentDescription = "설정 톱니바퀴",
-                                                    colorFilter = ColorFilter.tint(Gray500),
-                                                    contentScale = ContentScale.Crop,
-                                                )
-                                            }
-                                        }
-                                        // 마지막 아이템이 아닌 경우에만 Spacer를 추가
-                                        if (index < dummy.size - 1) {
-                                            Spacer(modifier = Modifier.height(4.dp))
+                                                }),
+                                            verticalAlignment = Alignment.CenterVertically
+                                        ) {
+                                            Text(
+                                                modifier = Modifier.padding(start = 16.dp),
+                                                text = item?.workspaceName ?:"",
+                                                style = MaterialTheme.typography.titleMedium
+                                            )
+                                            Spacer(modifier = Modifier.weight(1f))
+                                            Image(
+                                                modifier = Modifier
+                                                    .padding(end = 20.dp)
+                                                    .size(24.dp),
+                                                painter = painterResource(id = R.drawable.ic_expand_right_line),
+                                                contentDescription = "설정 톱니바퀴",
+                                                colorFilter = ColorFilter.tint(Gray500),
+                                                contentScale = ContentScale.Crop,
+                                            )
                                         }
                                     }
-
+                                    // 마지막 아이템이 아닌 경우에만 Spacer를 추가
+                                    if (index < dummy.size - 1) {
+                                        Spacer(modifier = Modifier.height(4.dp))
+                                    }
                                 }
+
                             }
                             Row(
                                 modifier = Modifier.padding(16.dp),
--- feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/model/WorkspaceDetailUiState.kt
+++ feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/model/WorkspaceDetailUiState.kt
@@ -3,8 +3,9 @@ package com.seugi.workspacedetail.model
 import com.seugi.data.workspace.model.WaitWorkspaceModel
 import com.seugi.data.workspace.model.WorkspaceModel
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 data class WorkspaceDetailUiState(
-    val myWorkspace: ImmutableList<WorkspaceModel?>? = null,
-    val waitWorkspace: ImmutableList<WaitWorkspaceModel?>? = null
+    val myWorkspace: ImmutableList<WorkspaceModel?> = listOf(WorkspaceModel()).toImmutableList(),
+    val waitWorkspace: ImmutableList<WaitWorkspaceModel?> = listOf(WaitWorkspaceModel()).toImmutableList()
 )
