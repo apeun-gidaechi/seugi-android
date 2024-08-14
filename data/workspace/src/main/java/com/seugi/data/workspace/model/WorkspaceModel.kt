6f3e1e8ee1857397fe6c6010813f85bb95acd267
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-14 14:40:14 +0900

    feat: persistentListOf() Reset

--- data/workspace/src/main/java/com/seugi/data/workspace/model/WorkspaceModel.kt
+++ data/workspace/src/main/java/com/seugi/data/workspace/model/WorkspaceModel.kt
@@ -1,6 +1,7 @@
 package com.seugi.data.workspace.model
 
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 
 data class WorkspaceModel(
@@ -8,7 +9,7 @@ data class WorkspaceModel(
     val workspaceName: String = "",
     val workspaceImageUrl: String = "",
     val workspaceAdmin: Long = 0,
-    val middleAdmin: ImmutableList<Long> = listOf<Long>(0).toImmutableList(),
-    val teacher: ImmutableList<Long> = listOf<Long>(0).toImmutableList(),
-    val student: ImmutableList<Long> = listOf<Long>(0).toImmutableList(),
+    val middleAdmin: ImmutableList<Long> = persistentListOf(),
+    val teacher: ImmutableList<Long> = persistentListOf(),
+    val student: ImmutableList<Long> = persistentListOf(),
 )
