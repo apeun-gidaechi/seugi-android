58652da706b726c1a1af951a19c23e43f690e438
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-14 17:15:17 +0900

    chore: persistentListOf() Reset

--- feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/feature/workspacedetail/model/WorkspaceDetailUiState.kt
+++ feature-main/workspace-detail/src/main/java/com/seugi/workspacedetail/feature/workspacedetail/model/WorkspaceDetailUiState.kt
@@ -3,13 +3,14 @@ package com.seugi.workspacedetail.feature.workspacedetail.model
 import com.seugi.data.workspace.model.WaitWorkspaceModel
 import com.seugi.data.workspace.model.WorkspaceModel
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 
 data class WorkspaceDetailUiState(
     val loading: Boolean = false,
     val nowWorkspace: WorkspaceModel = WorkspaceModel(),
-    val myWorkspace: ImmutableList<WorkspaceModel?> = listOf(WorkspaceModel()).toImmutableList(),
-    val waitWorkspace: ImmutableList<WaitWorkspaceModel?> = listOf(WaitWorkspaceModel()).toImmutableList(),
+    val myWorkspace: ImmutableList<WorkspaceModel?> = persistentListOf(),
+    val waitWorkspace: ImmutableList<WaitWorkspaceModel?> = persistentListOf(),
 )
 
 sealed interface WorkspaceDetailSideEffect {
