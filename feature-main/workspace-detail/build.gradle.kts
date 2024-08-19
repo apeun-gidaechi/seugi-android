dbb0d3d1b2cf6918c34d4688e4cf509910bd788d
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-20 07:25:08 +0900

    remove: Implementation

--- feature-main/workspace-detail/build.gradle.kts
+++ feature-main/workspace-detail/build.gradle.kts
@@ -10,8 +10,6 @@ dependencies {
     implementation(projects.designsystem)
     implementation(projects.data.member)
     implementation(projects.data.workspace)
-    // 이거 data.workspace가 data.core를 참조하는데 왜 profileModel()을 참조 못할까요
-    implementation(projects.data.core)
     implementation(projects.common)
     implementation(projects.ui)
 }
