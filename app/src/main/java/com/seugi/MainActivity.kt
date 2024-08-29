b930dd6b7783dce1677f423fd572a4b98c99a7c6
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-29 16:11:04 +0900

    style: Apply Spotless Format

--- app/src/main/java/com/seugi/MainActivity.kt
+++ app/src/main/java/com/seugi/MainActivity.kt
@@ -13,9 +13,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
