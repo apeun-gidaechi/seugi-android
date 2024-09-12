b792e6d94abff86df22205a10cf2787d89e343b5
8954sood <8954sood@naver.com>
2024-09-12 14:21:39 +0900

    style: Apply Spotless Format

--- feature-main/notification/src/main/java/com/seugi/notification/NotificationScreen.kt
+++ feature-main/notification/src/main/java/com/seugi/notification/NotificationScreen.kt
@@ -57,10 +57,10 @@ import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.makeappssimple.abhimanyu.composeemojipicker.ComposeEmojiPickerBottomSheetUI
 import com.seugi.common.utiles.toTimeString
-import com.seugi.data.notification.model.NotificationEmojiModel
 import com.seugi.data.core.model.WorkspacePermissionModel
 import com.seugi.data.core.model.isAdmin
 import com.seugi.data.core.model.isTeacher
+import com.seugi.data.notification.model.NotificationEmojiModel
 import com.seugi.data.notification.model.NotificationModel
 import com.seugi.designsystem.R
 import com.seugi.designsystem.animation.bounceClick
