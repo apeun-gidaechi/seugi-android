0700ea4e191c1942b5a59a1cd99dd1c8d7022e57
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-15 12:01:47 +0900

    feat: Insert Workspace Image

--- feature-main/workspace-create/src/main/java/com/seugi/workspacecreate/WorkspaceCreateScreen.kt
+++ feature-main/workspace-create/src/main/java/com/seugi/workspacecreate/WorkspaceCreateScreen.kt
@@ -1,9 +1,12 @@
 package com.seugi.workspacecreate
 
 import android.net.Uri
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,10 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.seugi.designsystem.R
+import com.seugi.designsystem.animation.bounceClick
 import com.seugi.designsystem.component.ButtonType
 import com.seugi.designsystem.component.SeugiFullWidthButton
 import com.seugi.designsystem.component.SeugiIconButton
@@ -100,19 +105,23 @@ fun WorkspaceCreateScreen(popBackStack: () -> Unit, viewModel: WorkspaceCreateVi
                             SeugiRoundedCircleImage(
                                 image = selectedImageUri.toString(),
                                 size = Size.Small,
-                                onClick = {},
+                                onClick = {
+                                    galleryLauncher.launch("image/*")
+                                }
                             )
                         } else {
                             SeugiRoundedCircleImage(
                                 size = Size.Small,
-                                onClick = {},
+                                onClick = {
+                                    galleryLauncher.launch("image/*")
+                                }
+                            )
+                            SeugiIconButton(
+                                resId = R.drawable.ic_add_fill,
+                                onClick = { galleryLauncher.launch("image/*") },
+                                colors = IconButtonDefaults.iconButtonColors(contentColor = SeugiTheme.colors.gray600),
                             )
                         }
-                        SeugiIconButton(
-                            resId = R.drawable.ic_add_fill,
-                            onClick = { galleryLauncher.launch("image/*") },
-                            colors = IconButtonDefaults.iconButtonColors(contentColor = SeugiTheme.colors.gray600),
-                        )
                     }
                 }
                 Row(
