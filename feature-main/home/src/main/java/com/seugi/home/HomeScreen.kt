927fd260eea869dbf84726e6d0542007a941dcdc
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-15 12:16:08 +0900

    feat: Create Home Pull Refresh

--- feature-main/home/src/main/java/com/seugi/home/HomeScreen.kt
+++ feature-main/home/src/main/java/com/seugi/home/HomeScreen.kt
@@ -22,11 +22,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -56,7 +63,9 @@ import com.seugi.home.card.ScheduleCard
 import com.seugi.home.card.SchoolCard
 import com.seugi.home.card.TimeScheduleCard
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class,
+    ExperimentalMaterialApi::class
+)
 @Composable
 internal fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
@@ -69,6 +78,17 @@ internal fun HomeScreen(
 ) {
     val view = LocalView.current
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    var isRefreshing by remember { mutableStateOf(false) }
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshing,
+        onRefresh = {
+            if (workspace.workspaceId.isEmpty()) {
+                return@rememberPullRefreshState
+            }
+            viewModel.load(workspace)
+        },
+    )
 
     val changeNavColorWhite = SeugiTheme.colors.white
     LifecycleResumeEffect(Unit) {
@@ -114,6 +134,11 @@ internal fun HomeScreen(
         )
     }
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState)
+    ){
     LazyColumn(
         modifier = Modifier
             .background(SeugiTheme.colors.primary050)
@@ -168,7 +193,15 @@ internal fun HomeScreen(
         item {
             Spacer(modifier = Modifier.height(32.dp))
         }
+
     }
+        PullRefreshIndicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            refreshing = isRefreshing,
+            state = pullRefreshState,
+        )
+    }
+
 }
 
 private fun changeNavigationColor(window: Window, backgroundColor: Color, isDark: Boolean) {
