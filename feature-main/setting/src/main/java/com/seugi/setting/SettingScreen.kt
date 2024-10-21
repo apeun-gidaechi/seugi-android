53d4b66c9d6be4eb825ab6b658d558ca78d0f558
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-21 18:58:24 +0900

    feat: Create Bottom Sheet

--- feature-main/setting/src/main/java/com/seugi/setting/SettingScreen.kt
+++ feature-main/setting/src/main/java/com/seugi/setting/SettingScreen.kt
@@ -1,22 +1,30 @@
 package com.seugi.setting
 
 import android.net.Uri
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeGesturesPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,11 +44,14 @@ import com.seugi.data.core.model.ProfileModel
 import com.seugi.designsystem.R
 import com.seugi.designsystem.animation.bounceClick
 import com.seugi.designsystem.component.AvatarType
+import com.seugi.designsystem.component.ButtonType
 import com.seugi.designsystem.component.DividerType
 import com.seugi.designsystem.component.LoadingDotsIndicator
 import com.seugi.designsystem.component.SeugiAvatar
 import com.seugi.designsystem.component.SeugiDivider
+import com.seugi.designsystem.component.SeugiFullWidthButton
 import com.seugi.designsystem.component.SeugiTopBar
+import com.seugi.designsystem.component.textfield.SeugiTextField
 import com.seugi.designsystem.theme.SeugiTheme
 import com.seugi.setting.model.SettingSideEffect
 import com.seugi.ui.CollectAsSideEffect
@@ -49,6 +60,7 @@ import com.seugi.ui.getFileSize
 import com.seugi.ui.getMimeType
 import com.seugi.ui.getUriByteArray
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SettingScreen(
     viewModel: SettingViewModel = hiltViewModel(),
@@ -62,6 +74,9 @@ internal fun SettingScreen(
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
     val context = LocalContext.current
     val contentResolver = context.contentResolver
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val modalBottomSheetState = rememberModalBottomSheetState()
+    var editText by remember { mutableStateOf("") }
 
     val galleryLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
@@ -75,6 +90,7 @@ internal fun SettingScreen(
                 fileMimeType = contentResolver.getMimeType(uri).toString(),
                 fileByteArray = contentResolver.getUriByteArray(uri),
                 fileByte = contentResolver.getFileSize(uri),
+                birth = profileModel.member.birth
             )
         }
     }
@@ -95,6 +111,57 @@ internal fun SettingScreen(
             SettingSideEffect.SuccessEdit -> {
                 reloadProfile()
                 showSnackbar("멤버 정보 변경 성공 !!")
+            }
+        }
+    }
+    if (showBottomSheet){
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = modalBottomSheetState,
+            dragHandle = { BottomSheetDefaults.DragHandle() },
+            containerColor = SeugiTheme.colors.white,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(SeugiTheme.colors.white)
+                    .safeGesturesPadding(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "이름 수정",
+                        style = SeugiTheme.typography.subtitle2,
+                        color = SeugiTheme.colors.black,
+                        modifier = Modifier.padding(start = 4.dp)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    SeugiTextField(
+                        value = editText,
+                        onValueChange = {
+                            editText = it
+                        },
+                        placeholder = "이름을 입력해주세요",
+                        onClickDelete = {
+                            editText = ""
+                        },
+                    )
+                }
+                Spacer(modifier = Modifier.height(32.dp))
+                SeugiFullWidthButton(
+                    onClick = {
+                        Log.d("TAG", "${profileModel.member.picture}: ")
+                        viewModel.editProfile(name = editText, profileImage = profileModel.member.picture, birth = profileModel.member.birth)
+                        showBottomSheet = false
+                    },
+                    type = ButtonType.Primary,
+                    text = "저장",
+                )
+                Spacer(modifier = Modifier.imePadding())
             }
         }
     }
@@ -167,13 +234,14 @@ internal fun SettingScreen(
                             modifier = Modifier
                                 .padding(vertical = 8.dp)
                                 .bounceClick(
-                                    onClick = {},
-                                    enabled = false,
+                                    onClick = {
+                                        showBottomSheet = true
+                                    }
                                 ),
                             verticalAlignment = Alignment.CenterVertically,
                         ) {
                             Text(
-                                text = profileModel.nick,
+                                text = "${profileModel.member.name} ${if (profileModel.nick.isEmpty()) "" else profileModel.nick}",
                                 style = SeugiTheme.typography.subtitle2,
                                 color = SeugiTheme.colors.black,
                             )
