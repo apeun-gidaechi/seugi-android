fa0ab072d7753a60b3b9d064c99bf4956b432fd9
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-25 09:26:12 +0900

    remove: Ai Navigation Button Effect

--- feature-main/home/src/main/java/com/seugi/home/card/CatSeugiCard.kt
+++ feature-main/home/src/main/java/com/seugi/home/card/CatSeugiCard.kt
@@ -3,6 +3,8 @@ package com.seugi.home.card
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +17,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -50,8 +53,10 @@ internal fun CatSeugiCard(uiState: CommonUiState<ImmutableList<String>>, navigat
                                 brush = GradientPrimary,
                                 shape = CircleShape,
                             )
-                            .bounceClick(
+                            .clickable (
                                 onClick = navigateToChatSeugi,
+                                indication = null,
+                                interactionSource = remember{ MutableInteractionSource() }
                             ),
                         verticalAlignment = Alignment.CenterVertically,
                     ) {
