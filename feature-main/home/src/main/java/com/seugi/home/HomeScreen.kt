a842272a8d1fc7013d4e70ce73bea66d4bec129d
8954sood <8954sood@naver.com>
2024-10-27 20:25:47 +0900

    style: Apply Spotless Format

--- feature-main/home/src/main/java/com/seugi/home/HomeScreen.kt
+++ feature-main/home/src/main/java/com/seugi/home/HomeScreen.kt
@@ -27,8 +27,8 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -159,7 +159,7 @@ internal fun HomeScreen(
                     containerColors = SeugiTheme.colors.primary050,
                 )
             },
-            containerColor = SeugiTheme.colors.primary050
+            containerColor = SeugiTheme.colors.primary050,
         ) {
             LazyColumn(
                 modifier = Modifier
